4927d4a6ed54e6d798d6549c099b67e6102406b5
Aleksejs Baranovs <A230556AB@rvt.lv>
2025-03-14 13:05:07 +0200

    Updated files

--- Wordle.py
+++ Wordle.py
@@ -10,7 +10,7 @@ import random
 app = Flask(__name__)
 console = Console()
 
-def select_random_word(file_path):
+def get_random_word(file_path):
     try:
         with open(file_path, 'r') as file:
             words = [word.strip() for word in file if len(word.strip()) == 5]
@@ -23,9 +23,24 @@ def select_random_word(file_path):
         print("File not found.")
         return None
 
-word_to_guess = select_random_word("C:\Users\A230556AB\Desktop\wordle\words\words_alpha.txt")
-print("Word to guess:", word_to_guess)
+word_to_guess = get_random_word("words\words_alpha.txt")
+
 
 
 correct_guesses = set()
 remaining_chances = 5
+
+
+def Displayed_word(word, correct_guess):
+    Display_word = ""
+    for letter in word:
+        Display_word += letter
+    else:
+        Display_word += "_"
+        return Display_word
+    
+def Guess(word, guess, correct_guess):
+    if guess.lower() ==word.lower():
+        correct_guess.update(word)
+        return True
+    return False
